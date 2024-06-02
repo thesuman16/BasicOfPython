03b61beed3da06e388c537bddb0cdb7d6d5523a7
suman160g <suman160g@gmail.com>
2024-06-02 05:43:48 +0000

    setup pu

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,9 +7,10 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
+     pkgs.python311
+     pkgs.python311Packages.pip
+     pkgs.nodejs_20
+     pkgs.python313Full
     # pkgs.nodePackages.nodemon
   ];
 
